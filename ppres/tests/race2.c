a3c60b38beb9c6db8b24e3095aa2005b3b336196
Steven Smith <sos22@cl.cam.ac.uk>
2010-01-22 11:28:02 +0000

    A test which doesn't have any actual races but stresses the locking routines.

---
+++ ppres/tests/race2.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+
+unsigned global;
+pthread_mutex_t mux;
+
+#define NR_LOOPS 100000
+
+static void
+thread(void)
+{
+  unsigned x;
+  for (x = 0; x < NR_LOOPS; x++) {
+    pthread_mutex_lock(&mux);
+    global++;
+    pthread_mutex_unlock(&mux);
+  }
+}
+
+int
+main()
+{
+  pthread_t pthr;
+  unsigned x;
+
+  pthread_mutex_init(&mux, NULL);
+  pthread_create(&pthr, NULL, thread, NULL);
+  for (x = 0; x < NR_LOOPS; x++) {
+    pthread_mutex_lock(&mux);
+    global *= 3;
+    pthread_mutex_unlock(&mux);
+  }
+  return global / 5;
+}
