5a51d050ecc70dd381acd7e7f3ea98048a191574
Steven Smith <sos22@srcf.ucam.org>
2010-01-23 18:17:49 +0000

    Oops, forgot a git add.

---
+++ ppres/driver/UIValue.hs
@@ -0,0 +1,12 @@
+module UIValue(uiv_destruct) where
+
+import Types
+import Worker
+
+instance Show UIValue where
+    show UIValueNull = "()"
+    show (UIValueSnapshot _) = "<snapshot>"
+
+uiv_destruct :: UIValue -> IO ()
+uiv_destruct UIValueNull = return ()
+uiv_destruct (UIValueSnapshot s) = killWorker s >> return ()
