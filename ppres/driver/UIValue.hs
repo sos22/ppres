d781809f4962039f3595c903507bd5453ce16cf0
Steven Smith <sos22@srcf.ucam.org>
2010-01-24 21:40:57 +0000

    Better formating of strings.

--- ppres/driver/UIValue.hs
+++ ppres/driver/UIValue.hs
@@ -6,7 +6,14 @@ instance Show UIValue where
     show UIValueNull = "()"
     show (UIValueSnapshot s) = show s
     show (UIValuePair a b) = "(" ++ (show a) ++ ", " ++ (show b) ++ ")"
-    show (UIValueList a) = "[" ++ (foldr (\x y -> x ++ "\n" ++ y) "]" $ map show a)
+    show (UIValueList a) =
+        let isChar (UIValueChar _) = True
+            isChar _ = False
+            gChar (UIValueChar c) = c
+            gChar _ = error "Huh?"
+        in if and $ map isChar a
+           then map gChar a
+           else "[" ++ (foldr (\x y -> x ++ "\n" ++ y) "]" $ map show a)
     show (UIValueChar c) = c:[]
     show (UIValueError e) = "ERR " ++ e
     show (UIValueTrace t) = "TRC " ++ show t
