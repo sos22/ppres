d7f3be7dfa077c10d44789fed8e608c028419391
Steven Smith <sos22@cl.cam.ac.uk>
2010-02-05 16:01:40 +0000

    Use a ruler function cache, rather than a FIFO one, because that means that we don't have to roll back all the way to the start quite as often.

--- ppres/driver/WorkerCache.hs
+++ ppres/driver/WorkerCache.hs
@@ -11,6 +11,8 @@ module WorkerCache(initWorkerCache, destroyWorkerCache, run,
 import Data.Word
 import Control.Monad.State
 import Data.IORef
+import Random
+import Control.Exception
 
 import System.IO.Unsafe
 
@@ -18,7 +20,11 @@ import Types
 import Worker
 import History
 
-data WorkerCache = WorkerCache { wc_workers :: [(History, Worker)],
+data CacheGeneration = CacheGeneration { cg_max_size :: Int,
+                                         cg_workers :: IORef [(History, Worker)],
+                                         cg_parent :: Maybe CacheGeneration }
+
+data WorkerCache = WorkerCache { wc_cache :: CacheGeneration,
                                  wc_start :: Worker }
 
 
@@ -27,34 +33,77 @@ globalWorkerCache :: IORef (Maybe WorkerCache)
 globalWorkerCache =
     unsafePerformIO $ newIORef Nothing
 
-sanityCheckWorkerCache :: WorkerCache -> IO ()
-sanityCheckWorkerCache wc =
-    mapM_ (\(_,w) -> do r <- readIORef $ worker_alive w
-                        if not r
-                           then error "found a dead worker in the cache"
-                           else return ()) $ wc_workers wc
-
 workerCache :: IO WorkerCache
 workerCache =
     do wc <- readIORef globalWorkerCache
        case wc of
          Nothing -> error "worker cache not ready"
-         Just wc' -> sanityCheckWorkerCache wc' >> return wc'
+         Just wc' -> return wc'
 
-setWorkerCache :: WorkerCache -> IO ()
-setWorkerCache = writeIORef globalWorkerCache . Just
+mkCacheGeneration :: Int -> IO CacheGeneration
+mkCacheGeneration sz =
+    do w <- newIORef []
+       parent <- if sz <= 2
+                 then return Nothing
+                 else liftM Just $ mkCacheGeneration $ sz `div` 2
+       return $ CacheGeneration { cg_max_size = sz,
+                                  cg_workers = w,
+                                  cg_parent = parent }
 
 initWorkerCache :: Worker -> IO ()
 initWorkerCache start =
-    writeIORef globalWorkerCache $ Just $ WorkerCache { wc_workers = [],
-                                                        wc_start = start }
+    do cg <- mkCacheGeneration 64
+       writeIORef globalWorkerCache $ Just $ WorkerCache { wc_cache = cg,
+                                                           wc_start = start }
+
+killCacheGeneration :: CacheGeneration -> IO ()
+killCacheGeneration cg =
+    do w <- readIORef $ cg_workers cg
+       mapM_ (killWorker . snd) w
+       case cg_parent cg of
+         Nothing -> return ()
+         Just x -> killCacheGeneration x
 
 destroyWorkerCache :: IO ()
 destroyWorkerCache =
     do wc <- workerCache
-       mapM_ (killWorker . snd) $ wc_workers wc
+       killCacheGeneration $ wc_cache wc
        killWorker $ wc_start wc
        return ()
+
+{- getBestWorkerForHist state: the best history found so far, and
+   something which gets the relevant worker and updates the cache. -}
+type GBState = (History, IO Worker)
+
+getBestWorkerForHist :: CacheGeneration -> History -> GBState -> IO GBState
+getBestWorkerForHist cg hist state =
+    let compareStates :: GBState -> GBState -> GBState
+        compareStates a@(hist1, _) b@(hist2, _) =
+            if not $ hist1 `historyPrefixOf` hist
+            then b
+            else if hist1 `historyPrefixOf` hist2
+                 then b
+                 else a
+    in
+    do {- Search parent caches first.  It doesn't actually matter which
+          order we use, since we're going to search the whole tree anyway. -}
+       state' <- case cg_parent cg of
+                   Nothing -> return state
+                   Just parent -> getBestWorkerForHist parent hist state
+       local_workers <- readIORef $ cg_workers cg
+       let entries :: [(History, Worker)] -> [(History, Worker)] -> [GBState]
+           entries [] _ = []
+           entries ((x@(xhist,worker)):xs) o =
+               (xhist, let nl = o ++ xs
+                       in do writeIORef (cg_workers cg) nl
+                             registerWorker xhist worker
+                             return worker):(entries xs (o ++ [x]))
+       return $ foldr compareStates state' $ entries local_workers []
+
+getBestWorker :: History -> IO GBState
+getBestWorker hist =
+    do cg <- liftM wc_cache workerCache
+       getBestWorkerForHist cg hist (emptyHistory, liftM wc_start workerCache)
 
 getWorker :: History -> IO (Bool, Worker)
 getWorker hist =
@@ -68,7 +117,8 @@ getWorker hist =
        completely by just forcing hist before doing anything -}
     force hist $
 
-    do (best_hist, best_worker) <- findBestWorker
+    do (best_hist, best_worker_io) <- getBestWorker hist
+       best_worker <- best_worker_io
        new_worker <- takeSnapshot best_worker
        let new_worker' = case new_worker of
                            Nothing ->
@@ -78,22 +128,6 @@ getWorker hist =
        return (cost > 10, {- 10 is pretty arbitrary, but seemed to work well
                              in some preliminary testing. -}
                new_worker')
-    where findBestWorker :: IO (History, Worker)
-          findBestWorker =
-              do wc <- workerCache
-                 return $ foldr compareWorkers
-                            (emptyHistory, wc_start wc)
-                            (wc_workers wc)
-              where
-                {- Compare two entries in the worker list, and return
-                   whichever one is better suited to the target
-                   history -}
-                compareWorkers a@(hist1, _) b@(hist2, _) =
-                    if not $ hist1 `historyPrefixOf` hist
-                    then b
-                    else if hist1 `historyPrefixOf` hist2
-                         then b
-                         else a
 
 
 registerWorker :: History -> Worker -> IO ()
@@ -104,11 +138,24 @@ registerWorker hist worker =
        worker cache. -}
     force hist $ force worker $
 
-    do wc <- workerCache
-       let (new_workers, dead_workers) =
-               splitAt 100 ((hist, worker):(wc_workers wc))
-       setWorkerCache $ wc { wc_workers = new_workers}
-       mapM_ (killWorker . snd) dead_workers
+    let registerWorkerCG :: Maybe CacheGeneration -> (History, Worker) -> IO ()
+        registerWorkerCG Nothing (_, w) = killWorker w
+        registerWorkerCG (Just cg) what =
+            do oldWorkers <- readIORef $ cg_workers cg
+               let newWorkers = what:oldWorkers
+                   nr_items_this_level = length newWorkers
+               if nr_items_this_level > cg_max_size cg
+                then assert (nr_items_this_level == cg_max_size cg + 1) $
+                     let (newNewWorkers, [dead]) = splitAt (cg_max_size cg) newWorkers
+                     in do r <- randomIO
+                           if r
+                            then registerWorkerCG (cg_parent cg) dead
+                            else killWorker $ snd dead
+                           writeIORef (cg_workers cg) newNewWorkers
+                else writeIORef (cg_workers cg) newWorkers
+    in
+    do cg <- liftM wc_cache $ workerCache
+       registerWorkerCG (Just cg) (hist, worker)
 
 
 traceCmd :: HistoryEntry -> History -> (Worker -> IO a) -> (History, a)
