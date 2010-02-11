2ffa5c16b1fa2db84776decbdc116aa82dac0204
Steven Smith <sos22@cl.cam.ac.uk>
2010-02-11 10:48:07 +0000

    Add the real clock time to timing log messages.

--- ppres/driver/Timing.hs
+++ ppres/driver/Timing.hs
@@ -6,6 +6,7 @@ import IO
 import System.IO.Unsafe
 import Data.IORef
 import CPUTime
+import Time
 
 timingFile :: Handle
 {-# NOINLINE timingFile #-}
@@ -15,18 +16,33 @@ lastEventTime :: IORef Integer
 {-# NOINLINE lastEventTime #-}
 lastEventTime = unsafePerformIO $ newIORef 0
 
+startTime :: ClockTime
+{-# NOINLINE startTime #-}
+startTime = unsafePerformIO getClockTime
+
+lastRTEvent :: IORef ClockTime
+lastRTEvent = unsafePerformIO $ newIORef startTime
+
 t :: IO Integer
 t = do x <- getCPUTime
        return $ x `div` 1000000
+
+tdToMicroseconds :: TimeDiff -> Integer
+tdToMicroseconds td = (toInteger $ tdSec td) * 1000000 + (tdPicosec td `div` 1000000)
 
 {- Add an entry to the timing log which shows the current time, the
    time delta, and the message. -}
 logentry :: String -> a -> IO a
 logentry msg val = do now <- t
+                      rtNow <- getClockTime
                       last_event <- readIORef lastEventTime
+                      rtLastEvent <- readIORef lastRTEvent
                       let time_since_last = now - last_event
-                      hPutStrLn timingFile $ (show now) ++ "\t" ++ (show time_since_last) ++ "\t" ++ msg
+                          rt_since_last = tdToMicroseconds $ rtNow `diffClockTimes` rtLastEvent
+                          rt_since_start = tdToMicroseconds $ rtNow `diffClockTimes` startTime
+                      hPutStrLn timingFile $ (show now) ++ "\t" ++ (show time_since_last) ++ "\t" ++ (show rt_since_start) ++ "\t" ++ (show rt_since_last) ++ "\t" ++ msg
                       writeIORef lastEventTime now
+                      writeIORef lastRTEvent rtNow
                       return val
 
 {- Semantically, this is const id, except that it transparently
