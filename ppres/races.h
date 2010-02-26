44cf8d7498ab0636984db3094b76ffc0e6b5cf07
Steven Smith <sos22@cl.cam.ac.uk>
2010-02-26 10:30:19 +0000

    Rip out the last vestiges of the old replay system.  It never really worked, anyway.

--- ppres/races.h
+++
@@ -1,7 +0,0 @@
-void racetrack_read_region(Addr start, unsigned size,  ThreadId this_thread);
-void racetrack_write_region(Addr start, unsigned size, ThreadId this_thread);
-void new_race_address(Addr addr);
-void mark_address_as_racy(Addr addr);
-void dump_racetrack_state(void);
-Bool racetrack_address_races(Addr start, unsigned size);
-void racetrack_thread_message(ThreadId sender, ThreadId receiver);
