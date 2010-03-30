5aa431d03b73ed9d1ba5690e6b8d2d1d17a445dc
Steven Smith <sos22@cl.cam.ac.uk>
2010-03-30 11:11:29 +0100

    Rework the debug log a bit.  Most importantly, stop reordering messages.  Also:
    -- Add a sequence number to messages, so that reordering is more
       obvious in future.
    -- Expose a way of producing log messages in the IO monad, since
       that's often useful.

--- ppres/driver/Debug.hs
+++ ppres/driver/Debug.hs
@@ -1,9 +1,9 @@
-module Debug(logMsg, dumpLog) where
+module Debug(logMsg, ioLogMsg, dumpLog) where
 
 import System.IO.Unsafe
 import Data.IORef
 
-data LogBuffer = LogBuffer { lb_strings :: [String],
+data LogBuffer = LogBuffer { lb_strings :: [(Integer, String)],
                              lb_nr_strings :: Int }
 
 nrLogBuffers :: Int
@@ -12,23 +12,29 @@ nrLogBuffers = 4
 nrStringsPerBuffer :: Int
 nrStringsPerBuffer = 1000
 
-globalLog :: IORef [LogBuffer]
+globalLog :: IORef ([LogBuffer], Integer)
 {-# NOINLINE globalLog #-}
 globalLog =
-    unsafePerformIO $ newIORef $ take nrLogBuffers $ repeat $ LogBuffer { lb_strings = [], lb_nr_strings = 0 }
+    unsafePerformIO $ newIORef (take nrLogBuffers $ repeat $ LogBuffer { lb_strings = [], lb_nr_strings = 0 },
+                                0)
+
+ioLogMsg :: String -> IO ()
+ioLogMsg msg =
+    do (b@(buffer:buffers), ident) <- readIORef globalLog
+       writeIORef globalLog
+        (if lb_nr_strings buffer < nrStringsPerBuffer
+         then ((LogBuffer { lb_strings = (ident, msg):(lb_strings buffer),
+                            lb_nr_strings = 1 + (lb_nr_strings buffer)}):buffers)
+         else ((LogBuffer { lb_strings = [(ident, msg)],
+                            lb_nr_strings = 1 }):(take (nrLogBuffers - 1) b)),
+         ident + 1)
 
 logMsg :: String -> a -> a
 logMsg msg val =
-    unsafePerformIO $ do b@(buffer:buffers) <- readIORef globalLog
-                         writeIORef globalLog $
-                          if lb_nr_strings buffer < nrStringsPerBuffer
-                          then ((LogBuffer { lb_strings = msg:(lb_strings buffer),
-                                             lb_nr_strings = 1 + (lb_nr_strings buffer)}):buffers)
-                          else ((LogBuffer { lb_strings = [msg],
-                                             lb_nr_strings = 1 }):(take (nrLogBuffers - 1) b))
+    unsafePerformIO $ do ioLogMsg msg
                          return val
 
 dumpLog :: IO ()
 dumpLog =
-    do buffers <- readIORef globalLog
-       mapM_ (\buffer -> mapM_ putStrLn $ reverse $ lb_strings buffer) buffers
+    do buffers <- fmap fst $ readIORef globalLog
+       mapM_ (\buffer -> mapM_ (\(a, b) -> putStrLn $ show a ++ ": " ++ b) $ reverse $ lb_strings buffer) $ reverse buffers
